1215167f6729c3f47a0121dbd0de23b6265ecc04
David <davitvrd@gmail.com>
2019-02-17 14:26:10 +0200

    Implement: add bindings for font awesome react package

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import * as React from 'react';
 import { Route, RouteComponentProps } from 'react-router-dom';
 
 import Header from './components/header';
+import './icons';
 
 class App extends React.Component {
   public render() {
---
+++ src/icons.ts
@@ -0,0 +1,24 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import {
+    faBuilding,
+    faUsers,
+    faFileAlt,
+    faFile,
+    faSortNumericDown,
+    faMoneyBill,
+    faTruck,
+    faEnvelope,
+    faPhone,
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+    faBuilding,
+    faUsers,
+    faFileAlt,
+    faSortNumericDown,
+    faMoneyBill,
+    faTruck,
+    faEnvelope,
+    faPhone,
+);
