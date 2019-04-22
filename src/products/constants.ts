49a81bd1f17b5fdebe939b9cc15d862915994824
David <davitvrd@gmail.com>
2019-04-22 12:13:56 +0300

    Fix: correct urls for prod/dev envs

--- src/products/constants.ts
+++ src/products/constants.ts
@@ -12,5 +12,5 @@ export const RESET_PRODUCTS = 'RESET_PRODUCTS';
 export type RESET_PRODUCTS = typeof RESET_PRODUCTS;
 
 // TODO: move this to some config...
-export const IMAGES_PATH_URL = 'http://95.213.203.160/media/images/';
+export const IMAGES_PATH_URL = (process.env.NODE_ENV === 'development' ? 'http://95.213.236.60/media/images/' : '/media/images/');
 export type IMAGES_PATH_URL = typeof IMAGES_PATH_URL;
